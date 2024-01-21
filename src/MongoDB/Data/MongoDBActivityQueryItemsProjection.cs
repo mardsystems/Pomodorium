12fc81ddf6f3253a80560e0a34c5b850997c502d
Marcelo Dias <mardsystems@gmail.com>
2024-01-21 18:20:24 -0300

    task: clarify data services

---
+++ src/MongoDB/Data/MongoDBActivityQueryItemsProjection.cs
@@ -0,0 +1,78 @@
+﻿using MediatR;
+using MongoDB.Driver;
+using TaskManagement.Features.ActivityManager;
+using TaskManagement.Models.Activities;
+
+namespace Pomodorium.Data;
+
+public class MongoDBActivityQueryItemsProjection :
+    IRequestHandler<ActivityQueryRequest, ActivityQueryResponse>,
+    INotificationHandler<ActivityCreated>,
+    INotificationHandler<ActivityUpdated>,
+    INotificationHandler<ActivityDeleted>
+{
+    private readonly MongoClient _mongoClient;
+
+    private readonly IMongoCollection<ActivityQueryItem> _mongoCollection;
+
+    public MongoDBActivityQueryItemsProjection(MongoClient mongoClient)
+    {
+        _mongoClient = mongoClient;
+
+        _mongoCollection = _mongoClient.GetDatabase("Pomodorium").GetCollection<ActivityQueryItem>("ActivityQueryItems");
+    }
+
+    public async Task<ActivityQueryResponse> Handle(ActivityQueryRequest request, CancellationToken cancellationToken)
+    {
+        var filter = Builders<ActivityQueryItem>.Filter.Empty;
+
+        var activityQueryItems = await _mongoCollection.Find(filter).ToListAsync(cancellationToken);
+
+        var response = new ActivityQueryResponse(request.GetCorrelationId())
+        {
+            ActivityQueryItems = activityQueryItems
+        };
+
+        return response;
+    }
+
+    public async Task Handle(ActivityCreated notification, CancellationToken cancellationToken)
+    {
+        var activityQueryItem = new ActivityQueryItem
+        {
+            Id = notification.ActivityId,
+            Name = notification.ActivityName,
+            State = notification.ActivityState,
+            StartDateTime = notification.StartDateTime,
+            StopDateTime = notification.StopDateTime,
+            Duration = notification.ActivityDuration,
+            Description = notification.ActivityDescription,
+            Version = notification.Version
+        };
+
+        await _mongoCollection.InsertOneAsync(activityQueryItem, null, cancellationToken);
+    }
+
+    public async Task Handle(ActivityUpdated notification, CancellationToken cancellationToken)
+    {
+        var filter = Builders<ActivityQueryItem>.Filter.Eq(x => x.Id, notification.ActivityId);
+
+        var update = Builders<ActivityQueryItem>.Update
+            .Set(x => x.Name, notification.ActivityName)
+            .Set(x => x.State, notification.ActivityState)
+            .Set(x => x.StartDateTime, notification.StartDateTime)
+            .Set(x => x.StopDateTime, notification.StopDateTime)
+            .Set(x => x.Duration, notification.ActivityDuration)
+            .Set(x => x.Description, notification.ActivityDescription)
+            .Set(x => x.Version, notification.Version);
+
+        await _mongoCollection.UpdateOneAsync(filter, update, null, cancellationToken);
+    }
+
+    public async Task Handle(ActivityDeleted notification, CancellationToken cancellationToken)
+    {
+        var filter = Builders<ActivityQueryItem>.Filter.Eq(x => x.Id, notification.ActivityId);
+
+        await _mongoCollection.DeleteOneAsync(filter, cancellationToken);
+    }
+}
---
+++ src/MongoDB/Data/MongoDBFlowtimeDetailsProjection.cs
@@ -0,0 +1,132 @@
+﻿using FlowtimeTechnique.Features.FlowTimer;
+using FlowtimeTechnique.Models;
+using MediatR;
+using MongoDB.Driver;
+using System.DomainModel;
+using TaskManagement.Models.Tasks;
+
+namespace Pomodorium.Data;
+
+public class MongoDBFlowtimeDetailsProjection :
+    IRequestHandler<FlowtimeDetailsRequest, FlowtimeDetailsResponse>,
+    INotificationHandler<FlowtimeCreated>,
+    INotificationHandler<FlowtimeStarted>,
+    INotificationHandler<FlowtimeInterrupted>,
+    INotificationHandler<FlowtimeStopped>,
+    INotificationHandler<TaskDescriptionChanged>,
+    INotificationHandler<FlowtimeArchived>
+{
+    private readonly MongoClient _mongoClient;
+
+    private readonly IMongoCollection<FlowtimeDetails> _mongoCollection;
+
+    public MongoDBFlowtimeDetailsProjection(MongoClient mongoClient)
+    {
+        _mongoClient = mongoClient;
+
+        _mongoCollection = _mongoClient.GetDatabase("Pomodorium").GetCollection<FlowtimeDetails>("FlowtimeDetails");
+    }
+
+    public async Task<FlowtimeDetailsResponse> Handle(FlowtimeDetailsRequest request, CancellationToken cancellationToken)
+    {
+        var filter = Builders<FlowtimeDetails>.Filter.Eq(x => x.Id, request.FlowtimeId);
+
+        var flowtimeDetails = await _mongoCollection.Find(filter).FirstAsync(cancellationToken) ?? throw new EntityNotFoundException();
+
+        var response = new FlowtimeDetailsResponse(request.GetCorrelationId())
+        {
+            FlowtimeDetails = flowtimeDetails
+        };
+
+        return response;
+    }
+
+    public async System.Threading.Tasks.Task Handle(FlowtimeCreated notification, CancellationToken cancellationToken)
+    {
+        var flowtimeDetails = new FlowtimeDetails
+        {
+            Id = notification.Id,
+            CreationDate = notification.CreationDate,
+            State = notification.State,
+            TaskId = notification.TaskId,
+            TaskDescription = notification.TaskDescription,
+            TaskVersion = notification.TaskVersion,
+            Version = notification.Version
+        };
+
+        await _mongoCollection.InsertOneAsync(flowtimeDetails, null, cancellationToken);
+    }
+
+    public async System.Threading.Tasks.Task Handle(FlowtimeStarted notification, CancellationToken cancellationToken)
+    {
+        var filter = Builders<FlowtimeDetails>.Filter.Eq(x => x.Id, notification.FlowtimeId);
+
+        var flowtimeDetails = await _mongoCollection.Find(filter).FirstAsync(cancellationToken) ?? throw new EntityNotFoundException();
+
+        var update = Builders<FlowtimeDetails>.Update
+            .Set(x => x.StartDateTime, notification.StartedAt)
+            .Set(x => x.State, notification.FlowtimeState)
+            .Set(x => x.Version, notification.Version);
+
+        await _mongoCollection.UpdateOneAsync(filter, update, null, cancellationToken);
+    }
+
+    public async System.Threading.Tasks.Task Handle(FlowtimeInterrupted notification, CancellationToken cancellationToken)
+    {
+        var filter = Builders<FlowtimeDetails>.Filter.Eq(x => x.Id, notification.Id);
+
+        var update = Builders<FlowtimeDetails>.Update
+            .Set(x => x.StopDateTime, notification.StopDateTime)
+            .Set(x => x.Interrupted, notification.Interrupted)
+            .Set(x => x.Worktime, notification.Worktime)
+            .Set(x => x.Breaktime, notification.Breaktime)
+            .Set(x => x.State, notification.State)
+            .Set(x => x.Version, notification.Version);
+
+        await _mongoCollection.UpdateOneAsync(filter, update, null, cancellationToken);
+    }
+
+    public async System.Threading.Tasks.Task Handle(FlowtimeStopped notification, CancellationToken cancellationToken)
+    {
+        var filter = Builders<FlowtimeDetails>.Filter.Eq(x => x.Id, notification.Id);
+
+        var flowtimeDetails = await _mongoCollection.Find(filter).FirstAsync(cancellationToken) ?? throw new EntityNotFoundException();
+
+        flowtimeDetails.StopDateTime = notification.StopDateTime;
+        flowtimeDetails.Interrupted = notification.Interrupted;
+        flowtimeDetails.Worktime = notification.Worktime;
+        flowtimeDetails.Breaktime = notification.Breaktime;
+        flowtimeDetails.State = notification.State;
+        flowtimeDetails.Version = notification.Version;
+
+        var update = Builders<FlowtimeDetails>.Update
+            .Set(x => x.StopDateTime, notification.StopDateTime)
+            .Set(x => x.Interrupted, notification.Interrupted)
+            .Set(x => x.Worktime, notification.Worktime)
+            .Set(x => x.Breaktime, notification.Breaktime)
+            .Set(x => x.State, notification.State)
+            .Set(x => x.Version, notification.Version);
+
+        await _mongoCollection.UpdateOneAsync(filter, update, null, cancellationToken);
+    }
+
+    public async System.Threading.Tasks.Task Handle(TaskDescriptionChanged notification, CancellationToken cancellationToken)
+    {
+        var filter = Builders<FlowtimeDetails>.Filter.Eq(x => x.TaskId, notification.TaskId);
+
+        var update = Builders<FlowtimeDetails>.Update
+            .Set(x => x.TaskDescription, notification.TaskDescription)
+            .Set(x => x.TaskVersion, notification.Version);
+
+        await _mongoCollection.UpdateManyAsync(filter, update, null, cancellationToken);
+    }
+
+    public async System.Threading.Tasks.Task Handle(FlowtimeArchived notification, CancellationToken cancellationToken)
+    {
+        var filter = Builders<FlowtimeDetails>.Filter.Eq(x => x.Id, notification.Id);
+
+        var _ = await _mongoCollection.Find(filter).FirstAsync(cancellationToken) ?? throw new EntityNotFoundException();
+
+        await _mongoCollection.DeleteOneAsync(filter, cancellationToken);
+    }
+}
